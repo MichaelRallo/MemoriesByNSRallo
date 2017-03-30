1cbb0ea3b08be1a6fea85ed58f32effab2dca872
Mike <rallomikework@gmail.com>
2017-03-30 14:51:35 -0500

    Mobiles

--- php/contact.php
+++ php/contact.php
@@ -1,8 +1,8 @@
 <?php
 
 // configure
-$from = 'rallomike@yahoo.com'; 
-$sendTo = 'rallomike@yahoo.com';
+$from = ''; 
+$sendTo = '';
 $subject = 'New message from contact form';
 $fields = array('name' => 'Name', 'surname' => 'Surname', 'phone' => 'Phone', 'email' => 'Email', 'message' => 'Message'); // array variable name => Text to appear in email
 $okMessage = 'Contact form successfully submitted. Thank you, I will get back to you soon!';
@@ -12,27 +12,16 @@ $errorMessage = 'There was an error while submitting the form. Please try again 
 
 try
 {
-    $message = "Line 1\r\nLine 2\r\nLine 3";
+    $emailText = "You have new message from contact form\n=============================\n";
 
-    // In case any of our lines are larger than 70 characters, we should use wordwrap()
-    $message = wordwrap($message, 70, "\r\n");
+    foreach ($_POST as $key => $value) {
 
-    // Send
-    mail('rallomike@yahoo.com', 'My Subject', $message);
-    $responseArray = array('type' => 'success', 'message' => $okMessage);
-    
-    
-    
-//    $emailText = "You have new message from contact form\n=============================\n";
-//
-//    foreach ($_POST as $key => $value) {
-//
-//        if (isset($fields[$key])) {
-//            $emailText .= "$fields[$key]: $value\n";
-//        }
-//    }
-//
-//    mail($sendTo, $subject, $emailText, "From: " . $from);
+        if (isset($fields[$key])) {
+            $emailText .= "$fields[$key]: $value\n";
+        }
+    }
+
+    mail($sendTo, $subject, $emailText, "From: " . $from);
 
     $responseArray = array('type' => 'success', 'message' => $okMessage);
 }
