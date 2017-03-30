409637578e7bcd3adc93d5d9df57a2975c36fe56
Mike <rallomikework@gmail.com>
2017-03-30 13:28:24 -0500

    Emails

---
+++ js/main.js
@@ -0,0 +1,12 @@
+function smoothScroll(x) {
+   $('html, body').animate({
+       scrollTop: $('#'+x+'Container').offset().top-50
+    }, 1200);
+}
+
+$('#nav li a').click(function(){
+    smoothScroll($(this).attr('id'));
+});
+
+
+
